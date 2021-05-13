3770f1794ceb14df0607d4b5456ef3734a8f32ee
Nebe <n3bb3z4r@gmail.com>
2021-05-13 20:54:19 +0200

    build ready for Vercel

--- pages/frontend.vue
+++ pages/frontend.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
     <div>
-      <p>HTML/CSS/JS, GIT/SVN, React/NeXT, VUE/NuXT.js, React Native, Python/Django, PHP/Laravel</p>
-      <p>Bulma/Tailwind/MaterialUI/Bootstrap/PureCSS, ThreeJS, UI/UX, Mockup & Wireframes, JIRA/Confluence/Kanban/Gantt/Slack/Trello/Discord</p>
+      <p>HTML/CSS/JS, GIT/SVN, React/NeXT, VUE/NuXT.js, React Native, Python/Django, PHP/Laravel/Symfony</p>
+      <p>Bulma/Tailwind/MaterialUI/Bootstrap/PureCSS/Gastby, ThreeJS, UI/UX, Mockup & Wireframes, JIRA/Confluence/Kanban/Gantt/Slack/Trello/Discord</p>
       <p>Heroku/Vercel, AWS/CDN, WAMP/XAMP/LAMP/MEAN, MySQL/MSSQL/Oracle, Node.js, ExpressJS, MongoDB/MariaDB, SSH/Bash</p>
       <p>Typescript, SASS/LESS/SCSS, Babel, ESLint, JEST, Pupeteer, AngularJS, Headless CMS/Wordpress/Joomla/Drupal</p>
     </div>
